8c9667af3896e2df6b17e6562de92283ad5f84f1
Denis Gathondu <thundoss@gmail.com>
2017-01-17 09:34:20 +0300

    Added prime.py and test_prime.py which contain the pseudocode of the functon and tests respectively

---
+++ prime.py
@@ -0,0 +1,7 @@
+# Take input
+# Confirm that the input is a number
+# Incase of floats, round the float to the nearest 10
+# Get the square root of the number
+# for each number from 2 to n(input) divide by m(square root of n)
+# if result of division is an integer n is not a prime, otherwise it is
+# return output stating if n is a prime or not
---
+++ test_prime.py
@@ -0,0 +1,10 @@
+# Test that invalid input throws a type error
+# Test that floats are rounded to nearest whole numbers
+# Test that input is a number greater than 1
+# Test that if input is less than 1 a value error is thrown
+# Test that the function gives correct output with 2 as input
+# Test that the function gives correct output with 3 as input
+# Test that the function gives correct output with 5 as input
+# Test that the function gives correct output with 16 as input
+# Test that the function gives correct output with 37 as input
+# Test that the function gives correct output with 166 as input
